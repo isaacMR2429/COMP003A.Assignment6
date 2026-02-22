c99ebbfcd5802b51d4e8684c5fcb46f508ae8d07
Isaac Martinez <isaacmartinez2429@gmail.com>
2026-02-22 15:16:54 -0800

    created the 3 objects from WorkoutSession.cs using the constructor and used different values for each object

--- COMP003A.Assignment6/Program.cs
+++ COMP003A.Assignment6/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" ");
+            WorkoutSession session1 = new WorkoutSession("Cardio Session", 30, false);
+            WorkoutSession session2 = new WorkoutSession("Weight Lifting", 60, true);
+            WorkoutSession session3 = new WorkoutSession("Stretching", 20, true);
         }
     }
 }
