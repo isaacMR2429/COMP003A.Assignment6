648bb75cc7e1a80b7eac543ea702696c8046e753
Isaac Martinez <isaacmartinez2429@gmail.com>
2026-02-22 15:28:56 -0800

    input a loop process object and menu interactions

--- COMP003A.Assignment6/Program.cs
+++ COMP003A.Assignment6/Program.cs
@@ -7,6 +7,16 @@
             WorkoutSession session1 = new WorkoutSession("Cardio Session", 30, false);
             WorkoutSession session2 = new WorkoutSession("Weight Lifting", 60, true);
             WorkoutSession session3 = new WorkoutSession("Stretching", 20, true);
+
+            bool working = true;
+            while (working)
+            {
+                Console.WriteLine("1. Display All Sessions");
+                Console.WriteLine("2. Update One Session");
+                Console.WriteLine("3. Exit");
+                Console.Write("Enter Choice: ");
+
+            }
         }
     }
 }
