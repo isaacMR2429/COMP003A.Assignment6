03d3ea8f9ece0f76540f44ad3348fac3d98adff9
Isaac Martinez <isaacmartinez2429@gmail.com>
2026-02-22 14:48:25 -0800

    stated 3 public fields and constructor

---
+++ COMP003A.Assignment6/WorkoutSession.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.Assignment6
+{
+    public class WorkoutSession
+    {
+        public string Name;
+        public int Time;
+        public bool Completed;
+
+        public WorkoutSession(string name, int time, bool completed)
+        {
+            Name = name;
+            Time = time;
+            Completed = completed;
+        }
+        
+    }
+}
